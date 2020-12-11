882476f4934b63f4bb88aec597c1d3eaac18f90e
Otávio <otavio@houpa.app>
2020-12-11 12:41:06 -0300

    feat: views and assets files

---
+++ views/home.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Teste</title>
+</head>
+<body>
+
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus aliquam quam corporis dolorem, explicabo mollitia excepturi cupiditate maxime, porro veritatis, nulla saepe laborum fugit dolor atque nemo? Vitae, hic unde!</p>
+
+</body>
+</html>
