a4ffcafd9ed675d5401d32080fed7a7f2b632cc3
otavio <otaviosilva2632@gmail.com>
2021-02-22 11:32:45 -0300

    feat: update path of minified assets

--- src/Config/Minify.php
+++ src/Config/Minify.php
@@ -5,10 +5,14 @@ use MatthiasMullie\Minify\JS;
 
 /** CSS minifier */
 $minCSS = new CSS();
-$minCSS->add(dirname(__DIR__, 1) . "/views/assets/css/style.css");
-$minCSS->minify(dirname(__DIR__, 1) . "/views/assets/style.min.css");
+$minCSS->add(dirname(__DIR__, 2) . "/resources/assets/css/style.css");
+
+/** Minified CSS file */
+$minCSS->minify(dirname(__DIR__, 2) . "/resources/assets/style.min.css");
 
 /** JS minifier */
 $minJS = new JS();
-$minJS->add(dirname(__DIR__, 1) . "/views/assets/js/scripts.js");
-$minJS->minify(dirname(__DIR__, 1) . "/views/assets/scripts.min.js");
+$minJS->add(dirname(__DIR__, 2) . "/resources/assets/js/scripts.js");
+
+/** Minified JS file */
+$minJS->minify(dirname(__DIR__, 2) . "/resources/assets/scripts.min.js");
