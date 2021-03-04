35c46862a87272f875d29f789331b6330a09d62f
otavio <otaviosilva2632@gmail.com>
2021-03-04 19:28:54 -0300

    feat: add verification to .env

--- src/Config/Constants.php
+++ src/Config/Constants.php
@@ -2,6 +2,10 @@
 
 use Dotenv\Dotenv;
 
+if (!file_exists(dirname(__DIR__, 2) . "/.env")) {
+  die("Your project not exist .env file. Copy .env.example and rename it to .env");
+}
+
 $dotenv = Dotenv::createImmutable(__DIR__ . "/../..");
 $dotenv->load();
 
