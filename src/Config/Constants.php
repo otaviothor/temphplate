8873070b56042b3198b4b37eaee16def6ad2552c
otavio <otaviosilva2632@gmail.com>
2021-03-05 16:43:27 -0300

    feat: changes

--- src/Config/Constants.php
+++ src/Config/Constants.php
@@ -1,10 +1,10 @@
 <?php
 
-use Dotenv\Dotenv;
-
 if (!file_exists(dirname(__DIR__, 2) . "/.env")) {
   die("Your project not exist .env file. Copy .env.example and rename it to .env");
 }
+
+use Dotenv\Dotenv;
 
 $dotenv = Dotenv::createImmutable(__DIR__ . "/../..");
 $dotenv->load();
