dcfa890708a1169d6d18654c1e5388e5939d1bc9
otavio <otaviosilva2632@gmail.com>
2021-03-16 08:18:03 -0300

    feat: finished class to manage sessions

--- src/Support/Session.php
+++ src/Support/Session.php
@@ -11,28 +11,52 @@ use stdClass;
  */
 abstract class Session
 {
-  public function get(string $index): string
+  public function __construct()
   {
-
+    if (!session_id()) {
+      session_start();
+    }
   }
 
-  public function put($something, string $value = null): void
+  public function get(string $index): ?string
   {
-
+    if (!empty($_SESSION[$index])) {
+      return $_SESSION[$index];
+    }
+    return null;
   }
 
-  public function all(): array
+  public function put($indexOrArray, $value = null): void
   {
-
+    if (sizeof($indexOrArray) > 1) {
+      foreach ($indexOrArray as $index => $value) {
+        $_SESSION[$index] = $value;
+        return;
+      }
+    }
+    $_SESSION[$indexOrArray] = $value;
+    return;
   }
 
-  public function destroy(string $index)
+  public function all(): ?array
   {
+    return $_SESSION;
+  }
 
+  public function destroy($indexOrArray)
+  {
+    if (sizeof($indexOrArray) > 1) {
+      foreach ($indexOrArray as $index) {
+        unset($_SESSION[$index]);
+        return;
+      }
+    }
+    unset($_SESSION[$indexOrArray]);
+    return;
   }
 
   public function exists(string $index): bool
   {
-
+    return isset($_SESSION[$index]);
   }
 }
