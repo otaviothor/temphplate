8584f9a13c3639193c2a5f08f87e9d82d00fc416
otavio <otaviosilva2632@gmail.com>
2021-03-17 19:21:07 -0300

    feat: add class to file system upload

--- src/Support/Upload.php
+++ src/Support/Upload.php
@@ -11,9 +11,30 @@ use stdClass;
  */
 abstract class Upload
 {
-  /** Upload constructor. */
-  public function __construct()
-  {
 
+  private $path;
+
+  /** Upload constructor. */
+  public function __construct(string $path)
+  {
+    $this->path = SITE["root"] . "/storage/{$path}/";
+  }
+
+  public function upload(string $name = null, string $tmpFile = null): bool
+  {
+    if ($name && $tmpFile && move_uploaded_file($tmpFile, $this->path . $name)) {
+      return true;
+    }
+
+    return false;
+  }
+
+  public function remove(string $name = null): bool
+  {
+    if ($name && unlink($this->path . $name)) {
+      return true;
+    }
+
+    return false;
   }
 }
